40d4b01c7180c7a5089dee7ce7dbd8e146fe1b24
Tiamat59 <test@test.com>
2020-02-20 12:35:00 +0100

    add modules

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,7 @@
-node default {
-  user { 'kermit':
-    ensure => present,
-    shell  => '/bin/bash'
-  }
-  file { '/home/kermit':
-    ensure => 'directory',
-    owner  => kermit,
-    group  => kermit,
-  }
-  file { '/home/kermit/.profile':
-    ensure  => 'present',
-    content => 'echo "I was created by puppet!"'
-  }
+node 'default' {
+  notify ("Hello depuis puppet")
+}
+
+node 'front' {
+  include muppet
 }
---
+++ modules/muppet/manifests/init.pp
@@ -0,0 +1,33 @@
+# Class: muppet
+#
+#
+class muppet {
+
+  group { 'kermit':
+    ensure => 'present'
+  }
+
+  user { 'kermit':
+    ensure  => 'present',
+    require => Group['kermit'],
+    gid     => 'kermit',
+    home    => '/home/kermit',
+    shell   => '/bin/bash',
+    before  => File['/home/kermit'],
+  }
+
+  file { '/home/kermit':
+    ensure => 'directory',
+    before => File['/home/kermit/.profile'],
+    owner  => 'kermit',
+    group  => 'kermit',
+  }
+
+  file { '/home/kermit/.profile':
+    ensure  => 'file',
+    content => 'echo "Hello from Kermit"',
+    owner   => 'kermit',
+    group   => 'kermit',
+  }
+
+}
